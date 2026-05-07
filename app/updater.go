sinspired/subs-check

feat: 修复版本自动更新的重启逻辑,相应的一些优化

app/updater.go
@@ -6,6 +6,7 @@ import (
 	"log/slog"
 	"os"
 	"os/exec"
+
 	"runtime"
 	"strings"
 	"syscall"
@@ -17,29 +18,71 @@ import (
 	"github.com/sinspired/subs-check/utils"
 )
 
-// restartSelf 跨平台重启
-func restartSelf() error {
-	exe, err := os.Executable()
-	if err != nil {
-		return err
+var originExePath string
+
+// InitUpdateInfo 检查是否为重启进程
+func (app *App) InitUpdateInfo() {
+	if os.Getenv("SUBS_CHECK_RESTARTED") == "1" {
+		slog.Info("版本更新成功")
+		// 清理环境变量
+		os.Unsetenv("SUBS_CHECK_RESTARTED")
 	}
+}
+
+// restartSelf 尝试跨平台自启
+func restartSelf(silentUpdate bool) error {
+	exe := originExePath
 
 	if runtime.GOOS == "windows" {
-		cmd := exec.Command(exe, os.Args[1:]...)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		cmd.Stdin = os.Stdin
-		cmd.Env = os.Environ()
-
-		// 关键：用 Run() 而不是 Start()
-		if err := cmd.Run(); err != nil {
-			return err
-		}
-		os.Exit(0)
+		return restartSelfWindows(exe)
 	}
 
-	// Unix-like 系统用 Exec 原地替换（前提：调用者已做清理）
+	// 非 Windows 平台使用 syscall.Exec
 	return syscall.Exec(exe, os.Args, os.Environ())
+}
+
+// restartSelfWindows Windows 平台重启方案
+func restartSelfWindows(exe string) error {
+	args := os.Args[1:]
+
+	// 转义参数
+	quotedArgs := make([]string, len(args))
+	for i, arg := range args {
+		// 如果参数包含空格或特殊字符，需要加引号
+		if strings.Contains(arg, " ") || strings.Contains(arg, "&") || strings.Contains(arg, "=") {
+			quotedArgs[i] = fmt.Sprintf(`"%s"`, arg)
+		} else {
+			quotedArgs[i] = arg
+		}
+	}
+
+	command := fmt.Sprintf(`timeout /t 1 /nobreak >nul && %s %s
+	`, exe, strings.Join(quotedArgs, " "))
+
+	// 启动批处理,不使用 START 命令，让批处理在当前控制台同步执行
+	cmd := exec.Command("cmd.exe", "/c", command)
+
+	// 继承当前控制台
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// 继承环境变量
+	cmd.Env = os.Environ()
+	// 添加重启标记
+	cmd.Env = append(cmd.Env, "SUBS_CHECK_RESTARTED=1")
+
+	slog.Info("新版本启动中...请勿关闭窗口！")
+
+	// 启动批处理（它会等待1秒后重启程序）
+	if err := cmd.Start(); err != nil {
+		// os.Remove(batPath)
+		return fmt.Errorf("启动重启脚本失败: %w", err)
+	}
+
+	// 立即退出当前进程
+	os.Exit(0)
+	return nil
 }
 
 // 清理系统代理环境变量
@@ -64,8 +107,18 @@ func tryUpdateOnce(ctx context.Context, updater *selfupdate.Updater, latest *sel
 	return updater.UpdateTo(ctx, latest, exe)
 }
 
+func (app *App) updateSuccess(current string, latest string, silentUpdate bool) {
+	slog.Info("更新成功，准备重启...")
+	app.Shutdown()
+	// 如配置了通知渠道,发送通知
+	utils.SendNotify_self_update(current, latest)
+	if err := restartSelf(silentUpdate); err != nil {
+		slog.Error("重启失败", "err", err)
+	}
+}
+
 // CheckUpdateAndRestart 检查更新并在需要时重启
-func (app *App) CheckUpdateAndRestart() {
+func (app *App) CheckUpdateAndRestart(silentUpdate bool) {
 	ctx := context.Background()
 
 	archMap := map[string]string{
@@ -92,6 +145,7 @@ func (app *App) CheckUpdateAndRestart() {
 	repo := selfupdate.NewRepositorySlug("sinspired", "subs-check")
 
 	// 先检测系统代理
+	slog.Info("更新代理环境")
 	isSysProxy := utils.GetSysProxy()
 
 	// 并发检测 GitHub Proxy
@@ -102,8 +156,9 @@ func (app *App) CheckUpdateAndRestart() {
 
 	// 第一次探测
 	updaterProbe, err := selfupdate.NewUpdater(selfupdate.Config{
-		Source: githubClient,
-		Arch:   arch,
+		Source:     githubClient,
+		Arch:       arch,
+		Prerelease: true, // 调试时允许预发布版本
 	})
 	if err != nil {
 		slog.Error("创建探测用 updater 失败", slog.Any("err", err))
@@ -125,9 +180,10 @@ func (app *App) CheckUpdateAndRestart() {
 
 	// 创建带校验器的 updater
 	updater, err := selfupdate.NewUpdater(selfupdate.Config{
-		Source:    githubClient,
-		Arch:      arch,
-		Validator: &selfupdate.ChecksumValidator{UniqueFilename: checksumFile},
+		Source:     githubClient,
+		Arch:       arch,
+		Validator:  &selfupdate.ChecksumValidator{UniqueFilename: checksumFile},
+		Prerelease: true, // 调试时允许预发布版本
 	})
 	if err != nil {
 		slog.Error("创建 updater 失败", slog.Any("err", err))
@@ -145,10 +201,7 @@ func (app *App) CheckUpdateAndRestart() {
 	}
 
 	// 比较版本
-	// currentVersion := app.version
-	// TODO: 调试时后使用正式变量
-	testVersion := "1.7.0"
-	currentVersion := testVersion
+	currentVersion := app.version
 
 	// 开发版逻辑：不更新，只提示
 	if strings.HasPrefix(currentVersion, "dev-") {
@@ -165,7 +218,7 @@ func (app *App) CheckUpdateAndRestart() {
 		return
 	}
 	if !latest.GreaterThan(curVer.String()) {
-		slog.Info("当前已是最新版本", slog.String("version", currentVersion))
+		slog.Info("已是最新版本", slog.String("version", currentVersion))
 		return
 	}
 
@@ -177,14 +230,15 @@ func (app *App) CheckUpdateAndRestart() {
 		return
 	}
 
+	// 避免linux系统重启路径错误
+	originExePath = exe
+
 	// 策略分支
 	if isSysProxy {
 		// 立即尝试系统代理
 		if err := tryUpdateOnce(ctx, updater, latest, exe,
 			latest.AssetURL, latest.ValidationAssetURL, false, "使用系统代理"); err == nil {
-			slog.Info("更新成功，准备重启...")
-			app.Shutdown()
-			_ = restartSelf()
+			app.updateSuccess(currentVersion, latest.Version(), silentUpdate)
 			return
 		}
 		// 等待 GitHub Proxy 结果（最多 10 秒）
@@ -199,17 +253,13 @@ func (app *App) CheckUpdateAndRestart() {
 			proxyAsset := ghProxy + latest.AssetURL
 			proxyValidation := ghProxy + latest.ValidationAssetURL
 			if err := tryUpdateOnce(ctx, updater, latest, exe, proxyAsset, proxyValidation, true, "使用 GitHub 代理"); err == nil {
-				slog.Info("更新成功，准备重启...")
-				app.Shutdown()
-				_ = restartSelf()
+				app.updateSuccess(currentVersion, latest.Version(), silentUpdate)
 				return
 			}
 		}
 		// 最后兜底
 		if err := tryUpdateOnce(ctx, updater, latest, exe, latest.AssetURL, latest.ValidationAssetURL, true, "原始链接兜底"); err == nil {
-			slog.Info("更新成功，准备重启...")
-			app.Shutdown()
-			_ = restartSelf()
+			app.updateSuccess(currentVersion, latest.Version(), silentUpdate)
 			return
 		}
 	} else {
@@ -219,18 +269,14 @@ func (app *App) CheckUpdateAndRestart() {
 			ghProxy := config.GlobalConfig.GithubProxy
 			proxyAsset := ghProxy + latest.AssetURL
 			proxyValidation := ghProxy + latest.ValidationAssetURL
-			if err := tryUpdateOnce(ctx, updater, latest, exe, proxyAsset, proxyValidation, false, "使用 GitHub 代理"); err == nil {
-				slog.Info("更新成功，准备重启...")
-				app.Shutdown()
-				_ = restartSelf()
+			if err := tryUpdateOnce(ctx, updater, latest, exe, proxyAsset, proxyValidation, true, "使用 GitHub 代理"); err == nil {
+				app.updateSuccess(currentVersion, latest.Version(), silentUpdate)
 				return
 			}
 		}
 		// 兜底
-		if err := tryUpdateOnce(ctx, updater, latest, exe, latest.AssetURL, latest.ValidationAssetURL, false, "原始链接兜底"); err == nil {
-			slog.Info("更新成功，准备重启...")
-			app.Shutdown()
-			_ = restartSelf()
+		if err := tryUpdateOnce(ctx, updater, latest, exe, latest.AssetURL, latest.ValidationAssetURL, true, "原始链接兜底"); err == nil {
+			app.updateSuccess(currentVersion, latest.Version(), silentUpdate)
 			return
 		}
 	}

jeplove/subs-check-pro

app/updater.go
@@ -6,6 +6,7 @@ import (
 	"log/slog"
 	"os"
 	"os/exec"
+
 	"runtime"
 	"strings"
 	"syscall"
@@ -17,29 +18,71 @@ import (
 	"github.com/sinspired/subs-check/utils"
 )
 
-// restartSelf 跨平台重启
-func restartSelf() error {
-	exe, err := os.Executable()
-	if err != nil {
-		return err
+var originExePath string
+
+// InitUpdateInfo 检查是否为重启进程
+func (app *App) InitUpdateInfo() {
+	if os.Getenv("SUBS_CHECK_RESTARTED") == "1" {
+		slog.Info("版本更新成功")
+		// 清理环境变量
+		os.Unsetenv("SUBS_CHECK_RESTARTED")
 	}
+}
+
+// restartSelf 尝试跨平台自启
+func restartSelf(silentUpdate bool) error {
+	exe := originExePath
 
 	if runtime.GOOS == "windows" {
-		cmd := exec.Command(exe, os.Args[1:]...)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		cmd.Stdin = os.Stdin
-		cmd.Env = os.Environ()
-
-		// 关键：用 Run() 而不是 Start()
-		if err := cmd.Run(); err != nil {
-			return err
-		}
-		os.Exit(0)
+		return restartSelfWindows(exe)
 	}
 
-	// Unix-like 系统用 Exec 原地替换（前提：调用者已做清理）
+	// 非 Windows 平台使用 syscall.Exec
 	return syscall.Exec(exe, os.Args, os.Environ())
+}
+
+// restartSelfWindows Windows 平台重启方案
+func restartSelfWindows(exe string) error {
+	args := os.Args[1:]
+
+	// 转义参数
+	quotedArgs := make([]string, len(args))
+	for i, arg := range args {
+		// 如果参数包含空格或特殊字符，需要加引号
+		if strings.Contains(arg, " ") || strings.Contains(arg, "&") || strings.Contains(arg, "=") {
+			quotedArgs[i] = fmt.Sprintf(`"%s"`, arg)
+		} else {
+			quotedArgs[i] = arg
+		}
+	}
+
+	command := fmt.Sprintf(`timeout /t 1 /nobreak >nul && %s %s
+	`, exe, strings.Join(quotedArgs, " "))
+
+	// 启动批处理,不使用 START 命令，让批处理在当前控制台同步执行
+	cmd := exec.Command("cmd.exe", "/c", command)
+
+	// 继承当前控制台
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// 继承环境变量
+	cmd.Env = os.Environ()
+	// 添加重启标记
+	cmd.Env = append(cmd.Env, "SUBS_CHECK_RESTARTED=1")
+
+	slog.Info("新版本启动中...请勿关闭窗口！")
+
+	// 启动批处理（它会等待1秒后重启程序）
+	if err := cmd.Start(); err != nil {
+		// os.Remove(batPath)
+		return fmt.Errorf("启动重启脚本失败: %w", err)
+	}
+
+	// 立即退出当前进程
+	os.Exit(0)
+	return nil
 }
 
 // 清理系统代理环境变量
@@ -64,8 +107,18 @@ func tryUpdateOnce(ctx context.Context, updater *selfupdate.Updater, latest *sel
 	return updater.UpdateTo(ctx, latest, exe)
 }
 
+func (app *App) updateSuccess(current string, latest string, silentUpdate bool) {
+	slog.Info("更新成功，准备重启...")
+	app.Shutdown()
+	// 如配置了通知渠道,发送通知
+	utils.SendNotify_self_update(current, latest)
+	if err := restartSelf(silentUpdate); err != nil {
+		slog.Error("重启失败", "err", err)
+	}
+}
+
 // CheckUpdateAndRestart 检查更新并在需要时重启
-func (app *App) CheckUpdateAndRestart() {
+func (app *App) CheckUpdateAndRestart(silentUpdate bool) {
 	ctx := context.Background()
 
 	archMap := map[string]string{
@@ -92,6 +145,7 @@ func (app *App) CheckUpdateAndRestart() {
 	repo := selfupdate.NewRepositorySlug("sinspired", "subs-check")
 
 	// 先检测系统代理
+	slog.Info("更新代理环境")
 	isSysProxy := utils.GetSysProxy()
 
 	// 并发检测 GitHub Proxy
@@ -102,8 +156,9 @@ func (app *App) CheckUpdateAndRestart() {
 
 	// 第一次探测
 	updaterProbe, err := selfupdate.NewUpdater(selfupdate.Config{
-		Source: githubClient,
-		Arch:   arch,
+		Source:     githubClient,
+		Arch:       arch,
+		Prerelease: true, // 调试时允许预发布版本
 	})
 	if err != nil {
 		slog.Error("创建探测用 updater 失败", slog.Any("err", err))
@@ -125,9 +180,10 @@ func (app *App) CheckUpdateAndRestart() {
 
 	// 创建带校验器的 updater
 	updater, err := selfupdate.NewUpdater(selfupdate.Config{
-		Source:    githubClient,
-		Arch:      arch,
-		Validator: &selfupdate.ChecksumValidator{UniqueFilename: checksumFile},
+		Source:     githubClient,
+		Arch:       arch,
+		Validator:  &selfupdate.ChecksumValidator{UniqueFilename: checksumFile},
+		Prerelease: true, // 调试时允许预发布版本
 	})
 	if err != nil {
 		slog.Error("创建 updater 失败", slog.Any("err", err))
@@ -144,19 +200,15 @@ func (app *App) CheckUpdateAndRestart() {
 		return
 	}
 
-	// 比较版本
-	// currentVersion := app.version
-	// TODO: 调试时后使用正式变量
-	testVersion := "1.7.0"
-	currentVersion := testVersion
-
 	// 开发版逻辑：不更新，只提示
-	if strings.HasPrefix(currentVersion, "dev-") {
+	if strings.HasPrefix(app.version, "dev") {
 		slog.Warn("当前为开发/调试版本，不执行自动更新")
 		slog.Info("最新版本", slog.String("version", latest.Version()))
 		slog.Info("手动更新", slog.String("url", latest.AssetURL))
 		return
 	}
+
+	currentVersion := app.originVersion
 
 	// 正式版逻辑：严格 semver 比较
 	curVer, err := semver.NewVersion(currentVersion)
@@ -165,7 +217,7 @@ func (app *App) CheckUpdateAndRestart() {
 		return
 	}
 	if !latest.GreaterThan(curVer.String()) {
-		slog.Info("当前已是最新版本", slog.String("version", currentVersion))
+		slog.Info("已是最新版本", slog.String("version", currentVersion))
 		return
 	}
 
@@ -177,14 +229,15 @@ func (app *App) CheckUpdateAndRestart() {
 		return
 	}
 
+	// 避免linux系统重启路径错误
+	originExePath = exe
+
 	// 策略分支
 	if isSysProxy {
 		// 立即尝试系统代理
 		if err := tryUpdateOnce(ctx, updater, latest, exe,
 			latest.AssetURL, latest.ValidationAssetURL, false, "使用系统代理"); err == nil {
-			slog.Info("更新成功，准备重启...")
-			app.Shutdown()
-			_ = restartSelf()
+			app.updateSuccess(currentVersion, latest.Version(), silentUpdate)
 			return
 		}
 		// 等待 GitHub Proxy 结果（最多 10 秒）
@@ -199,17 +252,13 @@ func (app *App) CheckUpdateAndRestart() {
 			proxyAsset := ghProxy + latest.AssetURL
 			proxyValidation := ghProxy + latest.ValidationAssetURL
 			if err := tryUpdateOnce(ctx, updater, latest, exe, proxyAsset, proxyValidation, true, "使用 GitHub 代理"); err == nil {
-				slog.Info("更新成功，准备重启...")
-				app.Shutdown()
-				_ = restartSelf()
+				app.updateSuccess(currentVersion, latest.Version(), silentUpdate)
 				return
 			}
 		}
 		// 最后兜底
 		if err := tryUpdateOnce(ctx, updater, latest, exe, latest.AssetURL, latest.ValidationAssetURL, true, "原始链接兜底"); err == nil {
-			slog.Info("更新成功，准备重启...")
-			app.Shutdown()
-			_ = restartSelf()
+			app.updateSuccess(currentVersion, latest.Version(), silentUpdate)
 			return
 		}
 	} else {
@@ -219,18 +268,14 @@ func (app *App) CheckUpdateAndRestart() {
 			ghProxy := config.GlobalConfig.GithubProxy
 			proxyAsset := ghProxy + latest.AssetURL
 			proxyValidation := ghProxy + latest.ValidationAssetURL
-			if err := tryUpdateOnce(ctx, updater, latest, exe, proxyAsset, proxyValidation, false, "使用 GitHub 代理"); err == nil {
-				slog.Info("更新成功，准备重启...")
-				app.Shutdown()
-				_ = restartSelf()
+			if err := tryUpdateOnce(ctx, updater, latest, exe, proxyAsset, proxyValidation, true, "使用 GitHub 代理"); err == nil {
+				app.updateSuccess(currentVersion, latest.Version(), silentUpdate)
 				return
 			}
 		}
 		// 兜底
-		if err := tryUpdateOnce(ctx, updater, latest, exe, latest.AssetURL, latest.ValidationAssetURL, false, "原始链接兜底"); err == nil {
-			slog.Info("更新成功，准备重启...")
-			app.Shutdown()
-			_ = restartSelf()
+		if err := tryUpdateOnce(ctx, updater, latest, exe, latest.AssetURL, latest.ValidationAssetURL, true, "原始链接兜底"); err == nil {
+			app.updateSuccess(currentVersion, latest.Version(), silentUpdate)
 			return
 		}
 	}